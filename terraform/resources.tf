c4886033f25763cbede1e1dd2ff475a492cd13f7
Oluwakayode <ojokayode13@gmail.com>
2025-11-23 11:16:24 +0100

    Modified the resources.tf file

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "raw_zone" {
-  bucket_prefix = "${var.project_name}-raw-zone-"
+  bucket        = "${var.project_name}-raw-zone-oluwakayode-dev"
   force_destroy = true
 }
 
